42f89a80ac2b0eb1ad15df833274fb4924026d0b
vmorenoluna <morenolunav@gmail.com>
2020-07-08 22:13:45 +0100

    Added MusicEvent

---
+++ src/main/scala/performance/MusicEvent.scala
@@ -0,0 +1,24 @@
+package performance
+
+import music.InstrumentName.InstrumentName
+import music.Types.{AbsPitch, Volume}
+import performance.Performance.{DurT, PTime}
+
+/**
+ * A music event
+ *
+ * @param eTime   onset time
+ * @param eInst   assigned instrument
+ * @param ePitch  pitch value 0-127
+ * @param eDur    note duration
+ * @param eVol    volume value 0-127
+ * @param eParams optional other parameters
+ */
+case class MusicEvent(
+                   eTime: PTime,
+                   eInst: InstrumentName,
+                   ePitch: AbsPitch,
+                   eDur: DurT,
+                   eVol: Volume,
+                   eParams: List[Double]
+                 )
