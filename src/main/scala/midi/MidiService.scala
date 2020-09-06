6291f715005c25dba3255e212bd3bd645a2db272
vmorenoluna <morenolunav@gmail.com>
2020-09-06 15:29:37 +0100

    Added MidiService

--- src/main/scala/midi/MidiService.scala
+++ src/main/scala/midi/MidiService.scala
@@ -7,7 +7,7 @@ import javax.sound.midi.ShortMessage._
 import performance.MusicEvent
 import performance.Performance.Performance
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 /**
  * Object that provides midi integration
@@ -15,17 +15,22 @@ import scala.util.Try
 object MidiService {
 
   val Resolution = 96
-  val currentTempoInBeatsPerMinute = 90
-  val ticksPerSecond = Resolution * (currentTempoInBeatsPerMinute / 60.0)
-  val tickSize = 1.0 / ticksPerSecond
+  private val sequencer: Try[Sequencer] = Try(MidiSystem.getSequencer())
+  private val synthesizer: Try[Synthesizer] = Try(MidiSystem.getSynthesizer())
 
+  //  val currentTempoBPM = 90
+  //  val ticksPerSecond = Resolution * (currentTempoBPM / 60.0)
+  //  val tickSize = 1.0 / ticksPerSecond
+
+  // TODO must accept a list of performances, so to use multiple instruments on the different channels
   def writePerformance(performance: Performance, pathName: String): Unit = {
     val events: Seq[MidiEvent] =
       performance
-        .map(eventToMidiEvents(_))
-        .flatMap(t => List(t._1, t._2))
+        .map(eventToMidiEvents)
+        .flatten
 
     val sequence: Sequence = new Sequence(Sequence.PPQ, Resolution)
+    // TODO One track per Part
     val track: Track = sequence.createTrack()
     for (ev <- events)
       track.add(ev)
@@ -34,10 +39,105 @@ object MidiService {
     MidiSystem.write(sequence, 1, file)
   }
 
-  private def eventToMidiEvents(event: MusicEvent): (MidiEvent, MidiEvent) = {
+
+  /**
+   * Plays the performance via the JavaSound MIDI synthesizer
+   *
+   * @param performance the performance to play
+   * @return
+   */
+  def play(performance: Performance): Unit = {
+    init()
+    performanceToMidiEvents(performance).map { sequence =>
+      sequencer match {
+        case Failure(exception) => handleError(ex = exception)
+        case Success(seq) => {
+          seq.open()
+          seq.addMetaEventListener((metaMsg: MetaMessage) => {
+            if (metaMsg.getType() == 0x2F) {
+              seq.close();
+            }
+          })
+          // seq.setTempoInBPM(m_masterTempo);
+          seq.setSequence(sequence)
+          seq.stop()
+          seq.setTickPosition(0)
+          seq.start();
+          seq.close()
+        }
+      }
+    }
+  }
+
+  /**
+   * Converts a Performance into a MIDI Sequence
+   *
+   * @param performance the performance
+   * @return Sequence to be played
+   */
+  private def performanceToMidiEvents(performance: Performance): Try[Sequence] =
+    Try {
+      // TODO handle program and control changes
+      // TODO One track per Part
+      // TODO Error handling
+
+      val events: Seq[MidiEvent] =
+        performance
+          .map(eventToMidiEvents)
+          .flatten
+
+      val sequence: Sequence = new Sequence(Sequence.PPQ, Resolution)
+      val track: Track = sequence.createTrack()
+      for (ev <- events) {
+        track.add(ev)
+      }
+
+      //        if (phrase.getInstrument() != NO_INSTRUMENT) instrument = phrase.getInstrument();
+      //        evt = createProgramChangeEvent(currChannel, instrument, phraseTick);
+      //        currTrack.add(evt);
+
+      //          if (pitch == Note.REST) {
+      //            phraseTick += note.getDuration() * m_ppqn * elementTempoRatio;
+      //            continue;
+      //          }
+
+      //          long onTick = (long)(phraseTick);
+      //           pan
+      //          if (note.getPan() != lastPanPosition) {
+      //            evt = createCChangeEvent(currChannel, 10, (int)(note.getPan()*127), onTick);
+      //            currTrack.add(evt);
+      //            lastPanPosition = note.getPan();
+      //          }
+
+      // add a meta event to indicate the end of the sequence.
+      // add a bit of time for reverb tail to fade if not cycling.
+      //    if (longestTime > 0.0 && longestTrack != null) {
+      //      MetaMessage msg = new MetaMessage();
+      //      byte [] data = new byte[0];
+      //      msg.setMessage(StopType, data, 0);
+      //      if (msCycle) {
+      //        MidiEvent evt = new MidiEvent(msg, (long)longestTime);
+      //        longestTrack.add(evt);
+      //      } else {
+      //        MidiEvent evt = new MidiEvent(msg, (long)(longestTime + 100)); //+ 100 if you want leave some space for reverb tail
+      //        longestTrack.add(evt);
+      //      }
+      //    }
+
+      sequence
+    }
+
+  /**
+   * Convert a MusicEvent to a list of MidiEvents
+   *
+   * @param event
+   * @return
+   */
+  private def eventToMidiEvents(event: MusicEvent): List[MidiEvent] = {
+    // TODO handle program and control changes
     val ev1 = createNoteOnEvent(0, event.ePitch, event.eVol, event.eTime.longValue)
     val ev2 = createNoteOffEvent(0, event.ePitch, event.eVol, (event.eTime + event.eDur).longValue)
-    (ev1.get, ev2.get)
+    List(ev1.get, ev2.get)
   }
 
   /**
@@ -94,6 +194,103 @@ object MidiService {
       val msg: ShortMessage = new ShortMessage(CONTROL_CHANGE, channel, controlNum, value)
       new MidiEvent(msg, tick)
     }
+
+  //  /** Pulses per quarter note value */
+  //  private short m_ppqn;
+  //  /** The Synthesizer we are using */
+  //  private Synthesizer m_synth;
+  //  /** The Sequence we are using */
+  //  private Sequence m_seq;
+  //  /** The Sequencer we are using */
+  //  private Sequencer m_sequencer;
+
+  //  /**
+  //   * Plays back the already computed sequencer via a MIDI synthesizer
+  //   * @exception Exception
+  //   */
+  //  private void rePlay() {
+  //    if (null == m_sequencer) {
+  //      if (!initSynthesizer()) {
+  //        return;
+  //      }
+  //    }
+  //    if (update) {
+  //      System.out.println("Updating playback sequence");
+  //      //Sequence seq;
+  //      try {
+  //        m_seq = scoreToSeq(updateScore);
+  //        if (null != m_seq) {
+  //          try {
+  //            m_sequencer.open();
+  //          }
+  //          catch (MidiUnavailableException e) {
+  //            System.err.println("MIDI System Unavailable:" + e);
+  //            return;
+  //          }
+  //          m_sequencer.setSequence(m_seq);
+  //        }
+  //      } catch (InvalidMidiDataException e) {
+  //        System.err.println("MIDISynth updating sequence error:" + e);
+  //        return;
+  //      }
+  //      update = false;
+  //    }
+  //    m_sequencer.setMicrosecondPosition(0l);
+  //    m_sequencer.setTempoInBPM(m_masterTempo);
+  //    m_sequencer.start();
+  //  }
+  //
+
+  /**
+   * Init the Midi Service
+   *
+   * @return true if the operation succeeded, false otherwise
+   */
+  def init(): Boolean =
+    sequencer match {
+      case Failure(ex) => handleError("Sequencer", ex)
+      case _ => initSyntesizer()
+    }
+
+  /**
+   * Teardown the Midi Service
+   *
+   * @return
+   */
+  def teardown(): Unit = {
+    sequencer.map(_.close())
+    synthesizer.map(_.close())
+  }
+
+  /**
+   * Check that the synthesizer has been properly
+   * created and opens it.
+   *
+   * @return
+   */
+  private def initSyntesizer(): Boolean = {
+    synthesizer match {
+      case Failure(ex) => handleError("Synthesizer", ex)
+      case Success(value) =>
+        Try(value.open()) match {
+          case Failure(ex) => handleError("Synthesizer", ex)
+          case _ => true
+        }
+    }
+  }
+
+  /**
+   * Handle a Java exception by printing
+   * the error on the console
+   *
+   * @param entity the midi entity that failed
+   * @param ex     the exception
+   * @return
+   */
+  private def handleError(entity: String = "", ex: Throwable): Boolean = {
+    println(s"MidiSystem Unavailable: [$entity] ${ex.getMessage}")
+    false
+  }
 
 }
 
