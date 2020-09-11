bbea24b636e87e05ad8ae427ca19f1aab7632823
vmorenoluna <morenolunav@gmail.com>
2020-09-11 20:38:53 +0100

    Updated Metronome

--- src/main/scala/performance/Metronome.scala
+++ src/main/scala/performance/Metronome.scala
@@ -1,22 +1,17 @@
 package performance
 
-import music.Types.Duration
-import performance.Performance.DurT
-
 /**
- * An object with a handy method to compute context's cDur
+ * An object with methods related to timing
  */
 object Metronome {
+
   /**
    * Calculate the ticked duration of one whole note
    *
-   * @param tempoInBPM is the tempo in BPM
-   * @param dur        the note type associated with one beat
+   * @param ticksPerQuarterNote   the note type associated with one beat
    * @return the ticked duration of one whole note
    */
-  def metro(tempoInBPM: Int, dur: Duration): DurT = {
-    val ticksPerBeat = 96
-    val ticksPerSecond = ticksPerBeat * (tempoInBPM / 60.0)
-    ticksPerSecond
-  }
+  def tickedWholeNote(ticksPerQuarterNote: Int): Int =
+    4 * ticksPerQuarterNote
+
 }
